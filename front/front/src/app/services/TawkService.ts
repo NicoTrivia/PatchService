a343106cf1399b2ddd2e5ba256b58406d65cf30a
fastlock <nicolas.bodin@triviatech.fr>
2023-05-31 01:31:23 +0200

    succeed to st name and tenant

--- front/front/src/app/services/TawkService.ts
+++ front/front/src/app/services/TawkService.ts
@@ -1,7 +1,9 @@
 import { DOCUMENT } from "@angular/common";
 import { Inject, Injectable, Renderer2, RendererFactory2 } from "@angular/core";
+
 // services
 import { AuthenticationService } from '../auth/authentication-service/authentication-service';
+
 declare var Tawk_API: any;
 
 @Injectable()
@@ -24,14 +26,13 @@ export class TawkService {
 
         const s = this.renderer.createElement('script');
         s.type = 'text/javascript';
-        s.text = `var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        s.text = `  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
         (function(){
         var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
         s1.async=true;
         s1.src='https://embed.tawk.to/6474d870ad80445890efbf86/1h1k6quae';
         s1.charset='UTF-8';
         s1.setAttribute('crossorigin','*');
-        s1.setAttribute('name', '${this.authenticationService.getUser()?.firstname} ${this.authenticationService.getUser()?.lastname}');
         s0.parentNode.insertBefore(s1,s0);
         })();
         `;
@@ -39,8 +40,16 @@ export class TawkService {
         Tawk_API.onLoad = this.loadedEvent.bind(this);
     }
 
-    private loadedEvent(){
+    private async loadedEvent(){
         this.loaded = true;
+       setTimeout(() => {
+            // Set visitor attributes
+            Tawk_API.setAttributes({
+                'name': this.authenticationService.getUser()?.firstname+' '+this.authenticationService.getUser()?.lastname+':'+this.authenticationService.getTenant(),
+                'Email': this.authenticationService.getUser()?.email,
+                'Organization': this.authenticationService.getTenant()
+            });
+       }, 1000);
     }
     
 }
